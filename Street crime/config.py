33755a3f17c021c311c990f2a7fe37bf0ddfbcd5
Garaway3139 <garaway3139@gmail.com>
2025-08-09 21:22:52 -0400

    Delete Street crime directory

--- Street crime/config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-SECRET_KEY = os.environ.get("SECRET_KEY", "change-me")
-DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///data.db")
-REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
--- Street crime/render-start.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-exec gunicorn -k eventlet -w 1 app:app --bind 0.0.0.0:$PORT
